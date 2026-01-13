a9efb53449469356e1ad752a97bfc47789292abd
Oscarf05 <ofchanataxi@espe.edu.ec>
2026-01-13 07:44:48 -0500

    Oauth

---
+++ autor-ms/src/main/java/ec/edu/espe/autorms/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package ec.edu.espe.autorms.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+public class IndexController {
+    @GetMapping("/authorized")
+    public Map<String, String> authorize(@RequestParam String code) {
+        return Collections.singletonMap("authorizationCode", code);
+    }
+}
