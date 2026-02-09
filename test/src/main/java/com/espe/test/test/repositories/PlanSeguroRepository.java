04809ec36f04ae241a3461d43077039c1dfc8e84
Oscarf05 <ofchanataxi@espe.edu.ec>
2026-02-09 17:37:44 -0500

    sdfsdfj

--- test/src/main/java/com/espe/test/test/repositories/PlanSeguroRepository.java
+++ test/src/main/java/com/espe/test/test/repositories/PlanSeguroRepository.java
@@ -1,7 +1,7 @@
 package com.espe.test.test.repositories; // <--- Verifica este paquete
 
 import com.espe.test.test.models.entities.PlanSeguro; // <--- Verifica este import
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PlanSeguroRepository extends CrudRepository<PlanSeguro, Long> {
+public interface PlanSeguroRepository extends JpaRepository<PlanSeguro, Integer> {
 }
